cbbab8f4a5cfb7877e5f4e2bc43d347c3e087de2
rabiasonmez <rabiasonmz@gmail.com>
2021-02-26 09:48:15 +0300

    Delete data directory

--- data/apiUrl.js
+++
@@ -1,7 +0,0 @@
-export const URL_LIST = "https://api.themoviedb.org/3/discover/movie";
-export const URL_SEARCH = "https://api.themoviedb.org/3/search/movie";
-export const URL_DETAIL = "https://api.themoviedb.org/3/movie/";
-export const URL_CREDITS = "/credits";
-export const URL_IMG = "https://image.tmdb.org/t/p/original/";
-export const API_KEY = "?api_key=bc16c9ee752ee15e0fe610b83d57f704";
-export const API_KEY_ALT = "&api_key=bc16c9ee752ee15e0fe610b83d57f704";
